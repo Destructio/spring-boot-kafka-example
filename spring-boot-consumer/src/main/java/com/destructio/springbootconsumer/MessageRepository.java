01e03ffef9aa25a7ced39e48ee6ab283221953dd
Destruction <htim44@gmail.com>
2024-08-06 22:35:59 +0300

    add MessageRepository as a storage for consumed messages

---
+++ spring-boot-consumer/src/main/java/com/destructio/springbootconsumer/MessageRepository.java
@@ -0,0 +1,27 @@
+package com.destructio.springbootconsumer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class MessageRepository {
+    private final ConcurrentHashMap<String, Message> messagesMap;
+
+    public MessageRepository() {
+        this.messagesMap = new ConcurrentHashMap<>();
+    }
+
+    public void save(String id, Message message) {
+        messagesMap.put(id, message);
+    }
+
+    public Optional<Message> findById(String id) {
+        return Optional.ofNullable(messagesMap.get(id));
+    }
+
+    public int count() {
+        return messagesMap.size();
+    }
+}
