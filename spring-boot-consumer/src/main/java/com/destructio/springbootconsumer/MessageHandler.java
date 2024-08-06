ba00561c9b9411062c8fd55f7582fa8898d18596
Destruction <htim44@gmail.com>
2024-08-06 22:36:30 +0300

    add Message save logic for MessageHandler

--- spring-boot-consumer/src/main/java/com/destructio/springbootconsumer/MessageHandler.java
+++ spring-boot-consumer/src/main/java/com/destructio/springbootconsumer/MessageHandler.java
@@ -1,15 +1,23 @@
 package com.destructio.springbootconsumer;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.kafka.support.KafkaHeaders.RECEIVED_KEY;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class MessageHandler {
+    private final MessageRepository messageRepository;
 
     @KafkaListener(topics = "messages")
-    private void printMessage(Message message) {
+    private void printMessage(@Header(RECEIVED_KEY) String key, @Payload Message message) {
         log.info("Received a new message! - {}", message.toString());
+        messageRepository.save(key, message);
     }
 }
