cfa7b5e087d2ec5bb22c098249ca88203a6b0d1a
Destruction <htim44@gmail.com>
2024-08-07 10:24:43 +0300

    refactor MessageSender

--- spring-boot-producer/src/main/java/com/destructio/springbootproducer/MessageSender.java
+++ spring-boot-producer/src/main/java/com/destructio/springbootproducer/MessageSender.java
@@ -19,16 +19,24 @@ public class MessageSender {
     private final KafkaTemplate<String, Message> kafkaTemplate;
     private static final Random random = new Random();
 
-    @Scheduled(fixedRate = 5000)
-    public void sendMessage() {
-        Message message = new Message(
-                "Sample Message title",
-                "Test " + random.nextInt(),
-                now(),
-                random.nextBoolean(),
-                random.nextLong()
-        );
+    public void sendMessage(Message message) {
         kafkaTemplate.send("messages", message)
                 .thenAccept((result -> log.info("Sent message! {}", result.getRecordMetadata().toString())));
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void sendGeneratedMessage() {
+        Message message = generateRandomMessage();
+        sendMessage(message);
+    }
+
+    private static Message generateRandomMessage() {
+        return new Message(
+                "Sample Message" + random.nextInt(900),
+                "Test " + random.nextInt(900),
+                now(),
+                random.nextBoolean(),
+                random.nextLong(10000)
+        );
     }
 }
