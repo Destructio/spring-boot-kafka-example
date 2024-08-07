f9164e1187bab9b5eea5344760a9d16c745e222a
Destruction <htim44@gmail.com>
2024-08-07 08:53:26 +0300

    fix messageTime creation in MessageSender

--- spring-boot-producer/src/main/java/com/destructio/springbootproducer/MessageSender.java
+++ spring-boot-producer/src/main/java/com/destructio/springbootproducer/MessageSender.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
-import static java.time.ZonedDateTime.now;
+import static java.time.LocalDateTime.now;
 
 @Slf4j
 @Service
